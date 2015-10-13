314d39e11934a61c365a009ac38a422dee980328
contendera <contendera@gmail.com>
2015-10-13 14:30:16 +0900

    1013

--- NoFapChallenge/app/src/main/java/com/changeandsuccess/nofapchallenge/profile_inside_tab/SettingsFrag.java
+++ NoFapChallenge/app/src/main/java/com/changeandsuccess/nofapchallenge/profile_inside_tab/SettingsFrag.java
@@ -1,17 +1,31 @@
 package com.changeandsuccess.nofapchallenge.profile_inside_tab;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TabHost;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.changeandsuccess.nofapchallenge.LoginHelper;
 import com.changeandsuccess.nofapchallenge.R;
 import com.changeandsuccess.nofapchallenge.model.LoginItem;
 import com.changeandsuccess.nofapchallenge.notify_stuff.CheckNotify;
 import com.changeandsuccess.nofapchallenge.notify_stuff.ToggleBellSend;
 import com.changeandsuccess.nofapchallenge.utils.UserDatabase;
+import com.nostra13.universalimageloader.core.DisplayImageOptions;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+import com.nostra13.universalimageloader.core.display.RoundedBitmapDisplayer;
+
+import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 
@@ -25,11 +39,12 @@ public class SettingsFrag extends Fragment {
 
     ImageButton notifyButton;
     String userIndex;
+
+    TextView title;
+    LinearLayout profileChangeBtn;
+
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
-
-
         //if logged in else
 
         UserDatabase userDatabase= new UserDatabase(getActivity());
@@ -37,12 +52,10 @@ public class SettingsFrag extends Fragment {
         String[][] data = userDatabase.getData();
         userDatabase.close();
 
-
         generatedLoginItem = generateData(data);
 
         if(generatedLoginItem.toString() !="[]"){
             userIndex = data[0][1];
-
         }else{
 
             userIndex ="1";
@@ -50,29 +63,55 @@ public class SettingsFrag extends Fragment {
 
         rootView = inflater.inflate(R.layout.settingsfrag_____settings_frag, container, false);
 
-
-
-        //check notify state
-
-        new CheckNotify(getActivity(), userIndex).execute();
-        //button
-/*
-        notifyButton = (ImageButton) rootView.findViewById(R.id.notify_button);
-        //onclick
-        notifyButton.setOnClickListener(new View.OnClickListener() {
+        title = (TextView)rootView.findViewById(R.id.textView);
+        profileChangeBtn = (LinearLayout)rootView.findViewById(R.id.profileChangeBtn);
+        profileChangeBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-
-               new ToggleBellSend(getActivity(), userIndex, rootView).execute();
-
-
+                //Toast.makeText(getActivity(),"클릭됨!!!!!",Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(getActivity(),UserInfoEdit.class);
+                startActivity(intent);
             }
         });
 
-*/
 
-return  rootView;
+        final TabHost tab_host = (TabHost)rootView.findViewById(R.id.tabhost);
+        tab_host.setup();
+
+        TabHost.TabSpec ts1 = tab_host.newTabSpec("tab1");
+        ts1.setIndicator("User Info");
+        ts1.setContent(R.id.tab1);
+        tab_host.addTab(ts1);
+
+        TabHost.TabSpec ts2 = tab_host.newTabSpec("tab2");
+        ts2.setIndicator("App Setting");
+        ts2.setContent(R.id.tab2);
+        tab_host.addTab(ts2);
+
+        tab_host.setOnTabChangedListener(new TabHost.OnTabChangeListener() {
+            @Override
+            public void onTabChanged(String tabId) {
+                if(tab_host.getCurrentTab()==0){
+                    title.setText("User Info");
+                }else if(tab_host.getCurrentTab()==1){
+                    title.setText("App Setting");
+                }
+            }
+        });
+
+        tab_host.setCurrentTab(0);
+
+
+        LoginHelper loginHelper = new LoginHelper();
+        String[][] loginData = loginHelper.checkLogin(getActivity());
+        ArrayList<LoginItem> generatedLoginItem = generateData(loginData);
+        if( generatedLoginItem.toString() !="[]"){
+            readSavedUser(getActivity(), generatedLoginItem);
+        }
+
+
+        new CheckNotify(getActivity(), userIndex).execute();
+        return  rootView;
     }//end on create
 
 
@@ -88,4 +127,45 @@ return  rootView;
         }//
         return items;
     } //end generate
+
+    public void readSavedUser(Context context, ArrayList<LoginItem> generatedLoginItem) {
+
+        TextView usernameText = (TextView) rootView.findViewById(R.id.profileID);
+      //  TextView lvText =  (TextView) rootView.findViewById(R.id.profileLevel);
+
+        String username = generatedLoginItem.get(0).getUsername().toString();
+      //  String lv = generatedLoginItem.get(0).getLevel().toString();
+
+        username = "ID : "+username;
+        usernameText.setText(username);
+     //   lv = "LV."+lv;
+     //   lvText.setText(lv);
+
+        String propic = generatedLoginItem.get(0).getProfile_picture();
+
+        ImageView profile_photo = (ImageView) rootView.findViewById(R.id.profilePicture);
+        String imageurl = "http://tanggoal.com/public/uploads/members_pic/" + propic;
+
+
+        ImageLoader imageloader = ImageLoader.getInstance();
+
+        DisplayImageOptions options = new DisplayImageOptions.Builder()
+                .showImageForEmptyUri(R.drawable.ic_launcher)
+                .displayer(new RoundedBitmapDisplayer(250))
+                .cacheInMemory(true)
+                .considerExifParams(true)
+                .build();
+
+        //kill it first
+
+        //imageloader.destroy();
+
+        Boolean isInit = imageloader.isInited();
+
+        if(!isInit) {
+            imageloader.init(ImageLoaderConfiguration.createDefault(context));
+        }
+
+        imageloader.displayImage(imageurl, profile_photo, options);
+    }
 }
---
+++ NoFapChallenge/app/src/main/java/com/changeandsuccess/nofapchallenge/profile_inside_tab/UserInfoEdit.java
@@ -0,0 +1,18 @@
+package com.changeandsuccess.nofapchallenge.profile_inside_tab;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+import com.changeandsuccess.nofapchallenge.R;
+
+/**
+ * Created by tanggames on 2015-10-13.
+ */
+public class UserInfoEdit extends ActionBarActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.settingsfrag_____user_info_edit);
+
+    }
+}
